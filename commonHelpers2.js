7e739baeeb753fd1f402b173c788f40f0071f0fb
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-02-27 17:01:36 +0000

    Deploying to gh-pages from @ Dima2430/book.store@527cf99fa5b4ff20cb7a7f651b4c5e8ce6c0cf1e 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,35 +1,33 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const o=document.querySelector(".slist-card-section"),g=document.querySelector(".slist-demo-thumb");let a=[];function m(s){try{for(let t=0;t<localStorage.length;t++){let i=localStorage.key(t);const l=localStorage.getItem(i),e=JSON.parse(l);a.push(e)}}catch{console.log(error)}}m();c(a);function v({id:s,book_image:t,title:i,list_name:l,description:e,author:n,amazonURL:r,appleURL:d}){return`<div class="slist-card-list">
+import"./assets/modulepreload-polyfill-3cfb730f.js";const e=document.querySelector(".slist-card-section"),g=document.querySelector(".slist-demo-thumb");let l=[];function m(s){try{for(let t=0;t<localStorage.length;t++){let i=localStorage.key(t);const a=localStorage.getItem(i),o=JSON.parse(a);l.push(o)}}catch{console.log(error)}}m();n(l);function u({id:s,book_image:t,title:i,list_name:a,description:o,author:c,amazonURL:r,appleURL:d}){return`<div class="slist-card-list">
             <div class="slist-card-item">
               <button type="button" data-id=${s} class="slist-del-btn js-slist-del-btn">
-                <svg class="slist-del-btn-img">
-                  <use href="./img/javascript.svg#trash"></use>
-                </svg>
               </button>
               <div class="slist-card-picture">
                 <img src="${t}" class="slist-book-img" alt="books" />
               </div>
               <div class="slist-info-container">
                 <h3 class="slist-book-header">${i}</h3>
-                <div class="slist-book-category">${l}</div>
-                <p class="slist-book-description">${e}</p>
-                <div class="slist-book-autor">${n}</div>
+                <div class="slist-book-category">${a}</div>
+                <p class="slist-book-description">${o}</p>
+                <div class="slist-book-autor">${c}</div>
 
                 <div class="slist-nav">
                   <ul class="slist-nav-list">
                     <li class="slist-nav-item">
                       <a href="${r}" class="slist-nav-link">
-                      <svg class="img-amazone">
-            <use href="./img/icons.svg#icon-amazon-pay"></use>
-          </svg></a>
+
+                      <img class="img-amazone logo" src="./img/amazon.png" alt="app">
+                     </a>
+
                     </li>
 
                     <li class="slist-nav-item">
-                      <a href="${d}" class="slist-nav-link"><img class="img-app" src="./img/ap.png" alt="app"></a>
+                      <a href="${d}" class="slist-nav-link"><img class="img-app logo" src="./img/ibook.png" alt="app"></a>
                       
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-          </div>`}function u(s){return s.map(v).join("")}function c(s){localStorage.length===0&&k();const t=u(s);o.innerHTML=t,o.addEventListener("click",p)}function p(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let t=s.target.dataset.id;localStorage.removeItem(s.target.dataset.id),a.filter(i=>i.id===t),a.splice(a.findIndex(i=>i.id===t),1),console.log(a),c(a)}}function k(){g.classList.remove("is-hidden")}
+          </div>`}function p(s){return s.map(u).join("")}function n(s){localStorage.length===0&&k();const t=p(s);e.innerHTML=t,e.addEventListener("click",v)}function v(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let t=s.target.dataset.id;localStorage.removeItem(s.target.dataset.id),l.filter(i=>i.id===t),l.splice(l.findIndex(i=>i.id===t),1),console.log(l),n(l)}}function k(){g.classList.remove("is-hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
