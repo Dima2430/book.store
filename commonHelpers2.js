cc801963c2aaa88339de38f0cc9e18ca50b6b0b8
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-02-28 18:17:52 +0000

    Deploying to gh-pages from @ Dima2430/book.store@c9a2d39603cdb762e56a886f3995537bcb3f58f9 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,14 +1,14 @@
-import"./assets/header-ec254fa6.js";const e="local-storage-books",m={id:"643282b1e85766588626a0dc",book_image:"https://storage.googleapis.com/du-prd/books/images/9781538748367.jpg",author:"Harlan Coben",list_name:"Audio Fiction",description:"A man imprisoned for murdering his 3-year-old son becomes convinced his son is still alive and plans an escape. Read by Steven Weber. 10 hours, 16 minutes unabridged.",title:"I WILL FIND YOU",amazonURL:"https://www.amazon.com/dp/1538748363?tag=NYTBSREV-20",appleURL:"https://goto.applebooks.apple/9781543661385?at=10lIEQ"};function p(){const s=localStorage.getItem(e);let t=s?JSON.parse(s):[];const a=m;t.every(i=>i.id!==a.id)&&t.push(a);const o=JSON.stringify(t);localStorage.setItem(e,o)}p();const n=document.querySelector(".slist-card-section"),u=document.querySelector(".slist-demo-thumb");function b(s){try{const t=localStorage.getItem(s);return JSON.parse(t)}catch{console.log("error"),l()}}const h=b(e);c(h);function v({id:s,book_image:t,title:a,list_name:o,description:i,author:r,amazonURL:d,appleURL:g}){return`<div class="slist-card-list">
+import"./assets/header-ec254fa6.js";const o="local-storage-books",a=document.querySelector(".slist-card-section"),m=document.querySelector(".slist-demo-thumb");function u(s){try{const l=localStorage.getItem(s);return JSON.parse(l)}catch{console.log("error"),n()}}const p=u(o);c(p);function v({id:s,book_image:l,title:i,list_name:t,description:e,author:r,amazonURL:d,appleURL:g}){return`<div class="slist-card-list">
             <div class="slist-card-item">
               <button type="button" data-id=${s} class="slist-del-btn js-slist-del-btn">
               </button>
               <div class="slist-card-picture">
-                <img src="${t}" class="slist-book-img" alt="books" />
+                <img src="${l}" class="slist-book-img" alt="books" />
               </div>
               <div class="slist-info-container">
-                <h3 class="slist-book-header">${a}</h3>
-                <h4 class="slist-book-category">${o}</h4>
-                <p class="slist-book-description">${i}</p>
+                <h3 class="slist-book-header">${i}</h3>
+                <h4 class="slist-book-category">${t}</h4>
+                <p class="slist-book-description">${e}</p>
                 <h5 class="slist-book-autor">${r}</h5>
 
                 <div class="slist-nav">
@@ -29,5 +29,5 @@ import"./assets/header-ec254fa6.js";const e="local-storage-books",m={id:"643282b
                 </div>
               </div>
             </div>
-          </div>`}function S(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!e)l();else{const t=S(s);n.innerHTML=t,n.addEventListener("click",k)}}async function k(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let t=s.target.dataset.id;const a=localStorage.getItem(e);let o=JSON.parse(a);if(localStorage.removeItem(e),o.splice(o.findIndex(i=>i.id===t),1),console.log(o),o.length<1)localStorage.removeItem(e),n.innerHTML="",l();else{const i=JSON.stringify(o);localStorage.setItem(e,i),c(o)}}}function l(){u.classList.remove("is-hidden")}
+          </div>`}function b(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!o)n();else{const l=b(s);a.innerHTML=l,a.addEventListener("click",h)}}async function h(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let l=s.target.dataset.id;const i=localStorage.getItem(o);let t=JSON.parse(i);if(localStorage.removeItem(o),t.splice(t.findIndex(e=>e.id===l),1),console.log(t),t.length<1)localStorage.removeItem(o),a.innerHTML="",n();else{const e=JSON.stringify(t);localStorage.setItem(o,e),c(t)}}}function n(){m.classList.remove("is-hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
