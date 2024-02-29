2c22d40497017dc832e8ae7c60353cea2b86d02d
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-02-29 20:49:18 +0000

    Deploying to gh-pages from @ Dima2430/book.store@5863fa5e53319377c31c1ede97c1620509db5006 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{S as e}from"./assets/header-be8c3040.js";const a=document.querySelector(".slist-card-section"),m=document.querySelector(".slist-demo-thumb");function u(s){try{const l=localStorage.getItem(s);return JSON.parse(l)}catch{console.log("error"),n()}}const p=u(e);c(p);function v({id:s,book_image:l,title:o,list_name:t,description:i,author:r,amazonURL:d,appleURL:g}){return`<div class="slist-card-list">
+import{S as i}from"./assets/header-1c5dacea.js";import"./assets/vendor-169f9f98.js";const a=document.querySelector(".slist-card-section"),g=document.querySelector(".slist-demo-thumb");function u(s){try{const l=localStorage.getItem(s);return JSON.parse(l)}catch{console.log("error"),n()}}const p=u(i);c(p);function v({_id:s,book_image:l,title:o,list_name:t,description:e,author:r,amazonURL:d,appleURL:m}){return`<div class="slist-card-list">
             <div class="slist-card-item">
               <button type="button" data-id=${s} class="slist-del-btn js-slist-del-btn">
               </button>
@@ -8,7 +8,7 @@ import{S as e}from"./assets/header-be8c3040.js";const a=document.querySelector("
               <div class="slist-info-container">
                 <h3 class="slist-book-header">${o}</h3>
                 <h4 class="slist-book-category">${t}</h4>
-                <p class="slist-book-description">${i}</p>
+                <p class="slist-book-description">${e}</p>
                 <h5 class="slist-book-autor">${r}</h5>
 
                 <div class="slist-nav">
@@ -22,12 +22,12 @@ import{S as e}from"./assets/header-be8c3040.js";const a=document.querySelector("
                     </li>
 
                     <li class="slist-nav-item">
-                      <a href="${g}" class="slist-nav-link"><img class="img-app logo" src="./img/ibook.png" alt="app"></a>
+                      <a href="${m}" class="slist-nav-link"><img class="img-app logo" src="./img/ibook.png" alt="app"></a>
                       
                     </li>
                   </ul>
                 </div>
               </div>
             </div>
-          </div>`}function b(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!e)n();else{const l=b(s);a.innerHTML=l,a.addEventListener("click",h)}}async function h(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let l=s.target.dataset.id;const o=localStorage.getItem(e);let t=JSON.parse(o);if(localStorage.removeItem(e),t.splice(t.findIndex(i=>i.id===l),1),console.log(t),t.length<1)localStorage.removeItem(e),a.innerHTML="",n();else{const i=JSON.stringify(t);localStorage.setItem(e,i),c(t)}}}function n(){m.classList.remove("is-hidden")}
+          </div>`}function b(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!i)n();else{const l=b(s);a.innerHTML=l,a.addEventListener("click",h)}}async function h(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let l=s.target.dataset.id;const o=localStorage.getItem(i);let t=JSON.parse(o);if(localStorage.removeItem(i),t.splice(t.findIndex(e=>e.id===l),1),console.log(t),t.length<1)localStorage.removeItem(i),a.innerHTML="",n();else{const e=JSON.stringify(t);localStorage.setItem(i,e),c(t)}}}function n(){g.classList.remove("is-hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
