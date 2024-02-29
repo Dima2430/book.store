7dfeda24b11c033519a4f0f73a0d743b3fee1c91
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-02-29 08:07:51 +0000

    Deploying to gh-pages from @ Dima2430/book.store@0198b521cf6ca4119406d31d38ddd012a0a7c9aa 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/header-1d09e868.js";const o="local-storage-books",a=document.querySelector(".slist-card-section"),m=document.querySelector(".slist-demo-thumb");function u(s){try{const l=localStorage.getItem(s);return JSON.parse(l)}catch{console.log("error"),n()}}const p=u(o);c(p);function v({id:s,book_image:l,title:i,list_name:t,description:e,author:r,amazonURL:d,appleURL:g}){return`<div class="slist-card-list">
+import{S as e}from"./assets/header-be8c3040.js";const a=document.querySelector(".slist-card-section"),m=document.querySelector(".slist-demo-thumb");function u(s){try{const l=localStorage.getItem(s);return JSON.parse(l)}catch{console.log("error"),n()}}const p=u(e);c(p);function v({id:s,book_image:l,title:o,list_name:t,description:i,author:r,amazonURL:d,appleURL:g}){return`<div class="slist-card-list">
             <div class="slist-card-item">
               <button type="button" data-id=${s} class="slist-del-btn js-slist-del-btn">
               </button>
@@ -6,9 +6,9 @@ import"./assets/header-1d09e868.js";const o="local-storage-books",a=document.que
                 <img src="${l}" class="slist-book-img" alt="books" />
               </div>
               <div class="slist-info-container">
-                <h3 class="slist-book-header">${i}</h3>
+                <h3 class="slist-book-header">${o}</h3>
                 <h4 class="slist-book-category">${t}</h4>
-                <p class="slist-book-description">${e}</p>
+                <p class="slist-book-description">${i}</p>
                 <h5 class="slist-book-autor">${r}</h5>
 
                 <div class="slist-nav">
@@ -29,5 +29,5 @@ import"./assets/header-1d09e868.js";const o="local-storage-books",a=document.que
                 </div>
               </div>
             </div>
-          </div>`}function b(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!o)n();else{const l=b(s);a.innerHTML=l,a.addEventListener("click",h)}}async function h(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let l=s.target.dataset.id;const i=localStorage.getItem(o);let t=JSON.parse(i);if(localStorage.removeItem(o),t.splice(t.findIndex(e=>e.id===l),1),console.log(t),t.length<1)localStorage.removeItem(o),a.innerHTML="",n();else{const e=JSON.stringify(t);localStorage.setItem(o,e),c(t)}}}function n(){m.classList.remove("is-hidden")}
+          </div>`}function b(s){return s.map(v).join("")}function c(s){if(s===null||s===[]||!e)n();else{const l=b(s);a.innerHTML=l,a.addEventListener("click",h)}}async function h(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let l=s.target.dataset.id;const o=localStorage.getItem(e);let t=JSON.parse(o);if(localStorage.removeItem(e),t.splice(t.findIndex(i=>i.id===l),1),console.log(t),t.length<1)localStorage.removeItem(e),a.innerHTML="",n();else{const i=JSON.stringify(t);localStorage.setItem(e,i),c(t)}}}function n(){m.classList.remove("is-hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
