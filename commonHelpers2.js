19ccbd6a8801d6277de46894461ad3afdc8a1a68
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-03-01 23:28:50 +0000

    Deploying to gh-pages from @ Dima2430/book.store@c1032def03b629b8b1af84ac179cc26dd7cabbd8 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -16,13 +16,13 @@ import"./assets/header-0053ffc9.js";import{a as m}from"./assets/vendor-169f9f98.
                     <li class="slist-nav-item">
                       <a href="${g}" class="slist-nav-link">
 
-                      <img class="img-amazone logo" src="./img/amazonicon.png" alt="app">
+                      <img class="img-amazone logo" src="./img/amazon.png" alt="app">
                      </a>
 
                     </li>
 
                     <li class="slist-nav-item">
-                      <a href="${d}" class="slist-nav-link"><img class="img-app logo" src="./img/appbook.png" alt="app"></a>
+                      <a href="${d}" class="slist-nav-link"><img class="img-app logo" src="./img/ibook.png" alt="app"></a>
                       
                     </li>
                   </ul>
