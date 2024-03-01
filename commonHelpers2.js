94f6cf8a503cf47f024ddaa050c40c1bd13386b3
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-03-01 22:08:48 +0000

    Deploying to gh-pages from @ Dima2430/book.store@09dbf41cb49ca9af16a56579dd80967bf07cead9 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,20 +1,20 @@
-import"./assets/header-0053ffc9.js";import{a as u}from"./assets/vendor-169f9f98.js";const i=document.querySelector(".slist-card-section"),p=document.querySelector(".slist-demo-thumb"),l="local-storage-books";let r=[];function b(s){try{const t=localStorage.getItem(s),o=JSON.parse(t);return console.log(o),r=o.map(a=>a.dataId),console.log(r),r}catch{console.log("error"),d()}}const k=b(l);g(k);async function g(s){let t=[];if(s===null||s.length===0||!l)d();else{for(const a of s){const e=await S(a);t.push(e)}const o=v(t);i.innerHTML=o,i.addEventListener("click",f)}}function h({_id:s,book_image:t,title:o,list_name:a,description:e,author:c,amazonURL:n,appleURL:m}){return`<div class="slist-card-list">
-            <div class="slist-card-item">
+import"./assets/header-0053ffc9.js";import{a as m}from"./assets/vendor-169f9f98.js";const r=document.querySelector(".slist-card-section"),u=document.querySelector(".slist-demo-thumb"),n="local-storage-books";let e=[];function p(s){try{const o=localStorage.getItem(s),t=JSON.parse(o);return console.log(t),e=t.map(l=>l.dataId),console.log(e),e}catch{console.log("error"),c()}}const b=p(n);console.log(e);k(b);async function k(s){let o=[];if(s===null||s.length===0||!n)c();else{for(const l of s){const a=await y(l);o.push(a)}const t=h(o);r.innerHTML=t,r.addEventListener("click",v)}}function f({_id:s,book_image:o,title:t,list_name:l,description:a,author:i,amazonURL:g,appleURL:d}){return`<div class="slist-card-list">
+            <li class="slist-card-item">
               <button type="button" data-id=${s} class="slist-del-btn js-slist-del-btn">
               </button>
               <div class="slist-card-picture">
-                <img src="${t}" class="slist-book-img" alt="books" />
+                <img src="${o}" class="slist-book-img" alt="books" />
               </div>
               <div class="slist-info-container">
-                <h3 class="slist-book-header">${o}</h3>
-                <h4 class="slist-book-category">${a}</h4>
-                <p class="slist-book-description">${e}</p>
-                <h5 class="slist-book-autor">${c}</h5>
+                <h3 class="slist-book-header">${t}</h3>
+                <h4 class="slist-book-category">${l}</h4>
+                <p class="slist-book-description">${a}</p>
+                <h5 class="slist-book-autor">${i}</h5>
 
                 <div class="slist-nav">
                   <ul class="slist-nav-list">
                     <li class="slist-nav-item">
-                      <a href="${n}" class="slist-nav-link">
+                      <a href="${g}" class="slist-nav-link">
 
                       <img class="img-amazone logo" src="./img/amazonicon.png" alt="app">
                      </a>
@@ -22,12 +22,12 @@ import"./assets/header-0053ffc9.js";import{a as u}from"./assets/vendor-169f9f98.
                     </li>
 
                     <li class="slist-nav-item">
-                      <a href="${m}" class="slist-nav-link"><img class="img-app logo" src="./img/appbook.png" alt="app"></a>
+                      <a href="${d}" class="slist-nav-link"><img class="img-app logo" src="./img/appbook.png" alt="app"></a>
                       
                     </li>
                   </ul>
                 </div>
               </div>
-            </div>
-          </div>`}function v(s){return s.map(h).join("")}async function f(s){if(s.target.nodeName==="BUTTON"){console.log(s.target.dataset.id);let t=s.target.dataset.id;const o=localStorage.getItem(l);let a=JSON.parse(o);if(a.length<=1)localStorage.removeItem(l),i.innerHTML="",d();else{localStorage.removeItem(l);let e=a.splice(a.findIndex(n=>n.id===t),1);i.innerHTML="";const c=JSON.stringify(e);localStorage.setItem(l,c),g(e.map(n=>n.dataId))}}}async function S(s){const t=`https://books-backend.p.goit.global/books/${s}`;try{return(await u.get(t)).data}catch{console.log("Результатів не знайдено.")}}function d(){p.classList.remove("is-hidden")}
+            </li>
+          </div>`}function h(s){return s.map(f).join("")}function v(s){if(s.target.nodeName!=="BUTTON")return;const o=s.target.closest("li");console.log(o);const t=s.target.dataset.id;console.log(t),o.remove(),S(t)}function S(s){const o=localStorage.getItem(n);let t=JSON.parse(o);t.length===0&&c(),console.log(t);let l=t.filter(i=>i.dataId===s);console.log(l);const a=JSON.stringify(l);localStorage.setItem(n,a)}async function y(s){const o=`https://books-backend.p.goit.global/books/${s}`;try{return(await m.get(o)).data}catch{console.log("Результатів не знайдено.")}}function c(){u.classList.remove("is-hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
